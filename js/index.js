7d69520f9d7baf4712d6ec2e52894ec3b280d0cd
JasSC97 <jaskaran2247@gmail.com>
2019-05-01 20:44:03 -0700

    Working carousel and preliminary implementation of the create account button. - Jas

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
 
-    $('.circleSelection').on('click', 'div', function(){
-        
+    
+    
+    
+    //The carousel selections are handled by the following lines of code.
+    $('#carouselSelection').on('click', '.circleSelection', function(){
         $('.currentSelection').addClass('circleSelection').removeClass('currentSelection');
         $(this).removeClass('circleSelection').addClass('currentSelection');
         
-    });    
+        if ($(this).is("#firstCarImage")) {
+            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselOne.jpg")');
+        }
+        if ($(this).is("#secondCarImage")) {
+            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselTwo.jpg")');
+        }
+        if ($(this).is("#thirdCarImage")) {
+            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselThree.jpg")');
+        }
+    });
+    
+    $('#createAccount').on('click', function() {
+        $('#landingPageWrap').hide();
+    });
+        
+    
     
     
 });
