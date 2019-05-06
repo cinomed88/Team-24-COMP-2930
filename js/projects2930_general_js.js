d5f73ecd7858763a48f2d736e2d16b1fa05c8302
DarkRagingInferno <47670645+DarkRagingInferno@users.noreply.github.com>
2019-05-06 16:55:01 -0700

    Added landscape navi-bar to gamepage & profile --Raging--

--- js/projects2930_general_js.js
+++ js/projects2930_general_js.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('body').prepend('<header></header');
     $('header').append('<div class="navi-bar-desktop"></div>');
     
-    $('.navi-bar-desktop').append('<a><div>Map</div></a>');
+    $('.navi-bar-desktop').append('<a href="./findgame.html"><div>Map</div></a>');
     $('.navi-bar-desktop').append('<a><div>Profile</div></a>');
     $('.navi-bar-desktop').append('<a><div>Store</div></a>');
     $('.navi-bar-desktop').append('<a><div>Sign-in</div></a>');
