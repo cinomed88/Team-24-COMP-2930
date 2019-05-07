ee97d4dabb5d988eac05bd7f7f4bebde2d4a3124
DarkRagingInferno <47670645+DarkRagingInferno@users.noreply.github.com>
2019-05-06 17:20:53 -0700

    Added links to the anchors for gamepage & profile --Raging--

--- js/general_2_stylesheet.js
+++ js/general_2_stylesheet.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
     $('body').prepend('<header></header');
     $('header').append('<div class="navi-bar-desktop"></div>');
     
-    $('.navi-bar-desktop').append('<a href="./findgame.html"><div>Map</div></a>');
-    $('.navi-bar-desktop').append('<a><div>Profile</div></a>');
+    $('.navi-bar-desktop').append('<a href="profile.html"><div>Profile</div></a>');
     $('.navi-bar-desktop').append('<a><div>Store</div></a>');
     $('.navi-bar-desktop').append('<a><div>Sign-in</div></a>');
-    $('.navi-bar-desktop').append('<a><div>Sign-out</div></a>');    
+    $('.navi-bar-desktop').append('<a href="./index.html"><div>Sign-out</div></a>');    
 });
