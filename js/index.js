a438c952e65ff258aef7200d914ba9be9d5b56f4
cinomed88 <gongthebest@gmail.com>
2019-05-10 11:32:03 -0700

    index css update

--- js/index.js
+++ js/index.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
         $(this).removeClass('circleSelection').addClass('currentSelection');
         
         if ($(this).is("#firstCarImage")) {
-            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselOne.jpg")');
+            $('#carousel').css('background-image', 'url("./Pics/Landing_Pics/carouselOne.jpg")');
         }
         if ($(this).is("#secondCarImage")) {
-            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselTwo.jpg")');
+            $('#carousel').css('background-image', 'url("./Pics/Landing_Pics/carouselTwo.jpg")');
         }
         if ($(this).is("#thirdCarImage")) {
-            $('#carousel').css('background-image', 'url("../Pics/Landing_Pics/carouselThree.jpg")');
+            $('#carousel').css('background-image', 'url("./Pics/Landing_Pics/carouselThree.jpg")');
         }
     });
     
