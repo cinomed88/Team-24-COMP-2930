abff4389a6deba3b2d33fb0e92eeae5c7b00d05d
DarkRagingInferno <47670645+DarkRagingInferno@users.noreply.github.com>
2019-05-20 02:10:58 -0700

    cleaned up some code in home.html & home_client.js -- Raging--

--- js/home_client.js
+++ js/home_client.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         data: {format: 'json-match-list'},
         success: function(data) {
             console.log("SUCCESS JSON:", data); 
-            for(let z = 0; z <= data.length && z <= 2; z++){
+            for(let z = 0; z <= data.length && z <= 3; z++){
                 let outerDiv = "";
                     outerDiv = document.createElement('div');
                     outerDiv.className += 'schedule' + " " + z;
@@ -61,41 +61,7 @@ $(document).ready(function(){
             }
         });
     });
-    scheduleContainerOverLay
-    
-    $('.test').on('click', function() {
-        $.ajax({
-            url: "/ajax-GET-data",
-            type: "GET",
-            dataType: "json",
-            data: {format: 'json-list'},
-            success: function(data) {
-                console.log("SUCCESS JSON:", data); 
-                    let outerDiv = "";
-                        for(let i = 2; i < 3; i++) {
-                        outerDiv = document.createElement('div');
-                        outerDiv.className += 'schedule' + i;
 
-                        var sport = document.createElement('div');
-                        sport.className += 'sport';
-
-                        var scheduleData = document.createElement('div');
-
-                            for(let k = 2; k < 3; k++) {
-                                scheduleData.className += 'scheduleData' + " " + k;
-                            }
-                        outerDiv.appendChild(sport);
-                        outerDiv.appendChild(scheduleData);
-
-                        $('.button').before(outerDiv);
-                    }
-                    console.log(data.sport);
-                    $('.2').html(data.sport);
-                }
-        });
-    });
-    
-    
     $('.test2').on('click', function() {
         $.ajax({
             url: "/ajax-GET-data",
