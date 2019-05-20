b9a9106d4e60e962aaf257f63aa2f81890978c41
DarkRagingInferno <47670645+DarkRagingInferno@users.noreply.github.com>
2019-05-20 13:09:08 -0700

    tweaked some buttons --Raging--

--- js/home_client.js
+++ js/home_client.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //START - Schedule container ajax
     $.ajax({
         url: "/ajax-GET-match-data",
         type: "GET",
@@ -16,7 +17,7 @@ $(document).ready(function(){
 
                     var scheduleData = document.createElement('div');
 
-                            scheduleData.className += 'scheduleData' + " " + z;
+                    scheduleData.className += 'scheduleData' + " " + z;
                     outerDiv.appendChild(sport);
                     outerDiv.appendChild(scheduleData);
 
@@ -69,12 +70,13 @@ $(document).ready(function(){
     $('.scheduleButtonOverLay').on('click', () => {
         $('.greaterOuterDiv').html('');
     });
-    
+    //END - schedule container ajax
+    //START - friends list ajax
     $.ajax({
-        url: "/ajax-GET-friends-data",
+        url: "/ajax-GET-match-data",
         type: "GET",
         dataType: "json",
-        data: {format: 'json-friends-list'},
+        data: {format: 'json-match-list'},
         success: function(data) {
             console.log("SUCCESS JSON:", data); 
             for(let z = 0; z <= data.length && z <= 3; z++){
@@ -83,15 +85,15 @@ $(document).ready(function(){
                     outerFriendsDiv.className += 'friends' + " " + z;
 
                     var avatar = document.createElement('div');
-                    sport.className += 'avatar' + " " + z;
+                    avatar.className += 'avatar' + " " + z;
 
                     var userNameData = document.createElement('div');
 
-                    scheduleData.className += 'userName' + " " + z;
-                    outerDiv.appendChild(avatar);
-                    outerDiv.appendChild(userNameData);
+                    userNameData.className += 'userName' + " " + z;
+                    outerFriendsDiv.appendChild(avatar);
+                    outerFriendsDiv.appendChild(userNameData);
 
-                    $('.friendsBTN').before(outerDiv);
+                    $('.friendsBTN').before(outerFriendsDiv);
                 
 //                console.log(data[z].user_name);
                 $(userNameData).html(data[z].user_name + "  " + data[z].honor_point);
@@ -99,12 +101,13 @@ $(document).ready(function(){
                 console.log('esketit', data);
         }
     });
+    
     $('.friendsButton').on('click', () => {
     $.ajax({
-        url: "/ajax-GET-friends-data",
+        url: "/ajax-GET-match-data",
         type: "GET",
         dataType: "json",
-        data: {format: 'json-friends-list'},
+        data: {format: 'json-match-list'},
         success: function(data) {
             console.log("SUCCESS JSON:", data); 
             let greaterOuterDiv = "";
@@ -117,13 +120,13 @@ $(document).ready(function(){
                     outerFriendsDiv.className += 'friendsOverLay' + " " + z;
 
                     var avatar = document.createElement('div');
-                    sport.className += 'avatarOverLay' + " " + z;
+                    avatar.className += 'avatarOverLay' + " " + z;
 
                     var userNameData = document.createElement('div');
 
-                            scheduleData.className += 'userNameOverLay' + " " + z;
-                    outerDiv.appendChild(avatar);
-                    outerDiv.appendChild(userNameData);
+                    userNameData.className += 'userNameOverLay' + " " + z;
+                    outerFriendsDiv.appendChild(avatar);
+                    outerFriendsDiv.appendChild(userNameData);
                     greaterOuterFriendsDiv.appendChild(outerFriendsDiv);
 
                     $('.friendsTitleOverLay').after(greaterOuterFriendsDiv);
@@ -140,38 +143,111 @@ $(document).ready(function(){
     $('.friendsButtonOverLay').on('click', () => {
         $('.greaterOuterFriendsDiv').html('');
     });
-    $('.test2').on('click', function() {
-        $.ajax({
-            url: "/ajax-GET-data",
-            type: "GET",
-            dataType: "json",
-            data: {format: 'json-list4'},
-            success: function(data) {
-                               console.log("Test2 SUCCESS JSON:", data);
-                var user = data; 
-                var firstName = user.firstName;
-                console.log(user);
-                let friendsOuterDiv = "";
-                for(let i = 1; i < 2; i++) {
-                    friendsOuterDiv = document.createElement('div');
-                    friendsOuterDiv.className += 'friends';
+    //END - friends list container ajax
+    //START - Recently played ajax
+    $.ajax({
+        url: "/ajax-GET-match-data",
+        type: "GET",
+        dataType: "json",
+        data: {format: 'json-match-list'},
+        success: function(data) {
+            console.log("SUCCESS JSON:", data); 
+            for(let z = 0; z <= data.length && z <= 3; z++){
+                let outerRecentlyDiv = "";
+                    outerRecentlyDiv = document.createElement('div');
+                    outerRecentlyDiv.className += 'recentlyPlayed' + " " + z;
 
-                    var avatar = document.createElement('div');
-                    avatar.className += 'avatar';
+                    var matchAvatar = document.createElement('div');
+                    matchAvatar.className += 'matchAvatar' + " " + z;
 
-                    var userName = document.createElement('div');
-                    userName.className += 'userName U2';
-
-                    friendsOuterDiv.appendChild(avatar);
-                    friendsOuterDiv.appendChild(userName);
-
-                    $('.friendsBTN').before(friendsOuterDiv);
-                }
-                console.log("does this output???", data);
-                console.log(data);
-                $('.userName').html(data.firstName);
+                    var matchUserData = document.createElement('div');
+                    matchUserData.className += 'matchUser' + " " + z;
                 
+                    outerRecentlyDiv.appendChild(matchAvatar);
+                    outerRecentlyDiv.appendChild(matchUserData);
+
+                    $('.recentBTN').before(outerRecentlyDiv);
+                
+//                console.log(data[z].user_name);
+                $(matchUserData).html(data[z].user_name + "  " + data[z].honor_point);
                 }
-            });
+                console.log('esketit', data);
+        }
+    });
+    $('.friendsButton').on('click', () => {
+    $.ajax({
+        url: "/ajax-GET-match-data",
+        type: "GET",
+        dataType: "json",
+        data: {format: 'json-match-list'},
+        success: function(data) {
+            console.log("SUCCESS JSON:", data); 
+            let greaterOuterRecentlyDiv = "";
+            greaterOuterRecentlyDiv = document.createElement('div');
+            greaterOuterRecentlyDiv.className += 'greaterOuterRecentlyDiv';
+            
+            for(let z = 0; z <= data.length; z++){
+                let outerRecentlyDiv = "";
+                    outerRecentlyDiv = document.createElement('div');
+                    outerRecentlyDiv.className += 'recentlyPlayed' + " " + z;
+
+                    var matchAvatar = document.createElement('div');
+                    matchAvatar.className += 'matchAvatarOverLay' + " " + z;
+
+                    var matchUserData = document.createElement('div');
+                    matchUserData.className += 'matchUserOverLay' + " " + z;
+                
+                    outerRecentlyDiv.appendChild(matchAvatar);
+                    outerRecentlyDiv.appendChild(matchUserData);
+                    greaterOuterRecentlyDiv.appendChild(outerRecentlyDiv);
+
+                    $('.recentPlayersOverLay').after(greaterOuterRecentlyDiv);
+                
+                console.log(data[z].user_name);
+                $(matchUserData).html(data[z].user_name + "  " + data[z].honor_point);
+                }
+                console.log('esketit', data);
+
+            }
         });
+    });
+    //Removes all divs from the friends container --Raging--
+    $('.friendsButtonOverLay').on('click', () => {
+        $('.greaterOuterFriendsDiv').html('');
+    });
+    //END - recently played ajax
+//    $('.test2').on('click', function() {
+//        $.ajax({
+//            url: "/ajax-GET-data",  
+//            type: "GET",
+//            dataType: "json",
+//            data: {format: 'json-list4'},
+//            success: function(data) {
+//                               console.log("Test2 SUCCESS JSON:", data);
+//                var user = data; 
+//                var firstName = user.firstName;
+//                console.log(user);
+//                let friendsOuterDiv = "";
+//                for(let i = 1; i < 2; i++) {
+//                    friendsOuterDiv = document.createElement('div');
+//                    friendsOuterDiv.className += 'friends';
+//
+//                    var avatar = document.createElement('div');
+//                    avatar.className += 'avatar';
+//
+//                    var userName = document.createElement('div');
+//                    userName.className += 'userName U2';
+//
+//                    friendsOuterDiv.appendChild(avatar);
+//                    friendsOuterDiv.appendChild(userName);
+//
+//                    $('.friendsBTN').before(friendsOuterDiv);
+//                }
+//                console.log("does this output???", data);
+//                console.log(data);
+//                $('.userName').html(data.firstName);
+//                
+//                }
+//            });
+//        });
     });
