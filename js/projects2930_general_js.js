9f2244a52996a27de8aad4da1c767e7445c192fb
JasSC97 <jaskaran2247@gmail.com>
2019-05-20 21:38:10 -0700

    Fixed the 'About Us' button on the navbar to redirect accordingly. -Jas

--- js/projects2930_general_js.js
+++ js/projects2930_general_js.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     
     $('.navi-bar-desktop').append('<a href="profile.html"><div>Profile</div></a>');
     $('.navi-bar-desktop').append('<a><div>Store</div></a>');
-    $('.navi-bar-desktop').append('<a><div>About Us</div></a>');
+    $('.navi-bar-desktop').append('<a href="aboutus.html"><div>About Us</div></a>');
     $('.navi-bar-desktop').append('<a href="#" onclick="firebase.auth().signOut()"><div>Sign-out</div></a>');
     
     $('header').after('<div class="navi-bar-mobile"></div>');
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $('.navi-bar-mobile').append('<div id="naviwrap"></div>');
     
     $('#naviwrap').append('<a href="#" onclick="firebase.auth().signOut()"><div class="sing-out">Sign-out</div></a>');
-    $('#naviwrap').append('<a><div>About Us</div></a>');
+    $('#naviwrap').append('<a href="aboutus.html"><div>About Us</div></a>');
     
      $('.homePageHamburger').click(function(){
         if($(this).hasClass('open') ){
