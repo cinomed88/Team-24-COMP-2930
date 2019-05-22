85294c759e2a4c9795991d84dc7d66e850e4bed7
cinomed88 <gongthebest@gmail.com>
2019-05-22 13:18:45 -0700

    order prob solved

--- app.js
+++ app.js
@@ -272,7 +272,7 @@ app.get('/ajax-GET-History', function (req, res) {
     // set the type of response:
     res.setHeader('Content-Type', 'application/json');
 
-    let qs2 = 'SELECT MATCH.match_id, lat, lng, time, date, sport, score, does_win FROM MATCH LEFT JOIN MATCH_PARTICIPANTS ON MATCH_PARTICIPANTS.match_id = MATCH.match_id WHERE MATCH_PARTICIPANTS.user_id =' + '\'' + q.query["name"] + '\'' + 'ORDER BY date, time DESC';
+    let qs2 = 'SELECT MATCH.match_id, lat, lng, time, date, sport, score, does_win FROM MATCH LEFT JOIN MATCH_PARTICIPANTS ON MATCH_PARTICIPANTS.match_id = MATCH.match_id WHERE MATCH_PARTICIPANTS.user_id =' + '\'' + q.query["name"] + '\'' + 'ORDER BY date DESC, time DESC';
     sequelize.query(qs2, { model: matches })
     .then(function (matchdata) {
         console.log(matchdata);
