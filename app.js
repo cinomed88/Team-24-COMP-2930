472af70c53ea83a8813303f0ceac191753f59d11
JasSC97 <jaskaran2247@gmail.com>
2019-05-22 10:36:43 -0700

    You should now only see matches that are on the current date or later on your schedules/reservations page. -Jas

--- app.js
+++ app.js
@@ -308,7 +308,7 @@ app.get('/ajax-GET-match-data', function (req, res) {
         let date = req.query['date'];
         res.setHeader('Content-Type', 'text/html');
         sequelize.query(`SELECT * FROM MATCH JOIN MATCH_PARTICIPANTS ON (MATCH.match_id = MATCH_PARTICIPANTS.match_id)
-WHERE MATCH_PARTICIPANTS.user_id = '${userId}' AND MATCH.date >= '${date}'`, { model: matches }).then(function(match) {
+WHERE MATCH_PARTICIPANTS.user_id = '${userId}' AND MATCH.date > '${date}'`, { model: matches }).then(function(match) {
             res.send(match);
             console.log(match);
         }).catch(function(err) {
